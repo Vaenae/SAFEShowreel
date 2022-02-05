671b36b7b934d27dce05a6b8bf9ff2439513d852
Vaenae <jvannine@gmail.com>
2022-02-05 13:49:22 +0200

    Dockerization

--- tests/E2E/Program.fs
+++ tests/E2E/Program.fs
@@ -1,8 +1,8 @@
 ﻿module E2ETests
 
 open Microsoft.Playwright
-// open FSharp.Control.Tasks
 open System.Threading.Tasks
+open System
 open Hopac
 open Logary
 open Logary.Configuration
@@ -17,43 +17,52 @@ type IPage with
             ()
         }
 
-let browserTest name (testFunction: IPage -> Task<unit>) = testTask name {
-    do! task {
-        use! web = Playwright.CreateAsync()
-        let! browser = web.Chromium.LaunchAsync(BrowserTypeLaunchOptions(Headless = true))
-        let! page = browser.NewPageAsync()
-        try
-            do! testFunction page
-            do! page.Screenshot($"success_{name}")
-        with ex ->
-            do! page.Screenshot($"failed_{name}")
-            raise ex
-        }
+let baseUrl =
+    match System.Environment.GetEnvironmentVariable("BASE_URL") with
+    | null -> "http://localhost:8080/"
+    | url -> url
+
+let browserTest name (testFunction: IPage -> Task<unit>) =
+    testTask name {
+        do!
+            task {
+                use! web = Playwright.CreateAsync()
+                let! browser = web.Chromium.LaunchAsync(BrowserTypeLaunchOptions(Headless = true))
+                let! page = browser.NewPageAsync()
+
+                try
+                    do! testFunction page
+                    do! page.Screenshot($"success_{name}")
+                with
+                | ex ->
+                    do! page.Screenshot($"failed_{name}")
+                    raise ex
+            }
     }
 
 [<Tests>]
-let testlist = testList "Server" [
-    testCase "A simple test" <| fun () ->
-        let expected = 4
-        Expect.equal expected (2+2) "2+2 = 4"
-    browserTest "Page title exists" <| fun page -> task {
-        let! _ = page.GotoAsync "http://localhost:8080/"
-        let! title = page.TextContentAsync "h1"
-        Expect.equal title "SAFEShowreel" "Title is SAFEShowreel"
-    }
-]
+let testlist =
+    testList
+        "Server"
+        [ browserTest "Page title exists"
+          <| fun page ->
+              task {
+                  let! _ = page.GotoAsync(url = baseUrl)
+                  let! title = page.TextContentAsync "h1"
+                  Expect.equal title "SAFEShowreel" "Title is SAFEShowreel"
+              } ]
 
 
 [<EntryPoint>]
 let main argv =
-  let logary =
-    Config.create "MyProject.Tests" "localhost"
-    |> Config.targets [ LiterateConsole.create LiterateConsole.empty "console" ]
-    |> Config.processing (Events.events |> Events.sink ["console";])
-    |> Config.build
-    |> run
-  LogaryFacadeAdapter.initialise<Expecto.Logging.Logger> logary
+    let logary =
+        Config.create "MyProject.Tests" "localhost"
+        |> Config.targets [ LiterateConsole.create LiterateConsole.empty "console" ]
+        |> Config.processing (Events.events |> Events.sink [ "console" ])
+        |> Config.build
+        |> run
 
-  // Invoke Expecto:
-  runTestsInAssemblyWithCLIArgs [] argv
+    LogaryFacadeAdapter.initialise<Expecto.Logging.Logger> logary
 
+    // Invoke Expecto:
+    runTestsInAssemblyWithCLIArgs [] argv
